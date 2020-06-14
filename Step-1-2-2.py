67521734b357c76839110fe6f48b95957f3571a7
Ashish Kumar <ashish@mitmuzaffarpur.org>
2020-06-14 23:47:15 +0530

    CO-PO Mapping : Step 2 Complete

--- Step-1-2-2.py
+++ Step-1-2-2.py
@@ -1,7 +1,12 @@
 import math
+from operator import add, truediv
 
 from CSVreadwrite import *
 
+Cos = ['CO1', 'CO2', 'CO3', 'CO4', 'CO5', 'CO6']
+POsPSOs_Original = ['PO1', 'PO2', 'PO3', 'PO4', 'PO5', 'PO6', 'PO7', 'PO8', 'PO9', 'PO10', 'PO11', 'PO12', 'PSO1',
+                    'PSO2', 'PSO3']
+avg_divider = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 pos = []
 cosDict = {}
 cosINpo = []
@@ -16,11 +21,11 @@ for i in range(1, 7):
     cosDict['CO' + str(i)] = data[i][3].split(',')
 
 # Total Session
-for i in range(1,7):
+for i in range(1, 7):
     totalSession += int(data[i][6])
 
 # Co - Session Dictionary
-for i in range(1,7):
+for i in range(1, 7):
     coWithSession[data[i][1]] = data[i][6]
 
 # List of POs and PSO
@@ -47,12 +52,12 @@ for po in pos:
     posINco[po] = cosINpo.copy()
     # print(posINco)
     # input("Press Enter to continue...")
+posINco_Original = posINco.copy()
 posINco_copy = posINco.copy()
 for po in posINco:
     temp = posINco_copy.get(po)
     temp = ','.join(temp)
     posINco_copy[po] = list(temp.split())
-
 
 # Co total Session for POs : To be Printed in Column 2
 for po in posINco:
@@ -65,7 +70,7 @@ for po in posINco:
 # % Session : To be printed in column 3
 posINco = posINco_copy.copy()
 for po in posINco:
-    temp = math.ceil((poSessions.get(po)/totalSession)*100)
+    temp = math.ceil((poSessions.get(po) / totalSession) * 100)
     posINco[po].append(str(poSessions.get(po)))
     posINco[po].append(str(temp))
     if temp >= 40:
@@ -76,20 +81,64 @@ for po in posINco:
         posINco[po].append('1')
     else:
         posINco[po].append('-')
+attn = {}
+# Co-PO Mapping Table
+for co in Cos:
+    L = []
+    K = []
+    for po in POsPSOs_Original:
+        if po in posINco_Original:
+            temp = posINco_Original[po]
+            # print(temp)
+            if co in temp:
+                L.append(int(posINco_copy.get(po)[3]))
+                K.append(1)
+            else:
+                L.append(0)
+                K.append(0)
+                # print(co,temp)
+        else:
+            L.append(0)
+            K.append(0)
+    attn[co] = L.copy()
+    L.clear()
+    avg_divider = list(map(add, avg_divider, K))
+    K.clear()
+    # print(attn)
+# CO-PO strength Average attn Average
+res_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+avg_list = []
+for co in Cos:
+    temp = attn[co]
+    res_list = list(map(add, res_list, temp))
+# totalCOsForAvg = 6
+# newList = [x / totalCOsForAvg for x in res_list]
+#avgListCOPO = list(map(truediv, res_list, avg_divider))
+print(res_list)
+for i in range(0, len(avg_divider)):
+    if avg_divider[i] is not 0:
+        res_list[i] /= avg_divider[i]
+avgListCOPO=res_list.copy()
+print(res_list)
+print(avg_divider)
+#print(avgListCOPO)
 
+attn['AVG'] = avgListCOPO
 
 print(pos)
 print(cosDict)
+print(posINco_Original)
 print(posINco)
 print(coWithSession)
 print(poSessions)
 print(posINco_copy)
+print(attn)
 
 # Simple list to save
 step2List = ["PO/PSO", "COs", "Total Session", "%Session", "Mapping Strength"]
 
 # Save it to Step -2
-with open('Step -2/POMapping.csv', 'w', newline='') as csv_file:
+with open('Step -2/COPOMapping.csv', 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(step2List)
     step2List.clear()
@@ -98,4 +147,14 @@ with open('Step -2/POMapping.csv', 'w', newline='') as csv_file:
         step2List.extend((value))
         writer.writerow(step2List)
         step2List.clear()
-       # writer.writerow([key, value])
+    # writer.writerow([key, value])
+
+with open('Step -2/COPOMapping.csv', 'a', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow("\n")
+    step2List = []
+    for key, value in attn.items():
+        step2List.append(key)
+        step2List.extend((value))
+        writer.writerow(step2List)
+        step2List.clear()
