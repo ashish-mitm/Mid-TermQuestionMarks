1feac02eeb92ddcb6636f2baf147ee198fa409ab
Ashish Kumar <ashish@mitmuzaffarpur.org>
2020-06-18 13:12:19 +0530

    Upto All

--- clkcMarksFilePath.py
+++ clkcMarksFilePath.py
@@ -1,3 +1,3 @@
 
-clkcFilePath = "FIT/FIT-CLKC.csv"
-MarksFilePath = "FIT/18/2nd Sem/InfoFIT-18-IT-ME-LT-CE.csv"
+clkcFilePath = "CO-PO-MIT/15 Batch IT/5/FLAT/FLAT-CLKC.csv"
+MarksFilePath = "CO-PO-MIT/15 Batch IT/5/FLAT/Info51511.csv"
